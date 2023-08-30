750751c7e89e72a12a00a64f994427822b1dd50d
Jibran Gulzar <97299110+Jibranwani@users.noreply.github.com>
2023-08-30 09:49:05 +0530

    Update Additionnum.java

--- Addition/src/Additme/Additionnum.java
+++ Addition/src/Additme/Additionnum.java
@@ -5,7 +5,7 @@ public class Additionnum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 1;
-		int b =2;
+		int b =7;
 		int c = a+b;
 		System.out.println("the sum is: "+c);
 
